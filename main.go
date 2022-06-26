ed3cf42a6a5b4418f1160fdd4c7a6f4759cfa719
Maxime Leriche <maxleriche.60@gmail.com>
2022-06-26 23:57:35 +0200

    Basic handle of the keycloak realm

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"batleforc/keycloakoperator/model"
 	"context"
+	"crypto/tls"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -21,15 +22,45 @@ func main() {
 	if err2 != nil {
 		log.Fatal(err2)
 	}
-	for k, v := range data {
-		fmt.Printf("%s -> %d\n", k, v)
+	client := gocloak.NewClient("http://localhost:8080", gocloak.SetAuthAdminRealms("admin/realms"), gocloak.SetAuthRealms("realms"))
+	restyClient := client.RestyClient()
+	restyClient.SetTLSClientConfig(&tls.Config{InsecureSkipVerify: true})
+	ctx := context.Background()
+	token, err := client.LoginAdmin(ctx, "admin", "admin", "master")
+	if err != nil {
+		panic("Login failed:" + err.Error())
+	}
+	for _, v := range data {
+		if !v.RealmExist(client, token.AccessToken) {
+			name, success, err3 := v.CreateRealm(client, token.AccessToken)
+			if success {
+				log.Printf("%s => Has been succesfuly created or exist \n", name)
+			} else {
+				log.Printf("%s => %s \n", name, err3)
+			}
+		} else {
+			err3 := v.UpdateRealm(client, token.AccessToken)
+			if err3 == nil {
+				log.Printf("%s => Has been succesfuly updated \n", v.Name)
+			} else {
+				log.Printf("%s => %s \n", v.Name, err3)
+			}
+		}
+
+	}
+	realms, err := client.GetRealms(ctx, token.AccessToken)
+	if err != nil {
+		panic("GetRealm failed:" + err.Error())
+	}
+	for _, realm := range realms {
+		fmt.Printf("%s\n", *realm.Realm)
 	}
 }
 
 func KeyCloakTest() {
 	client := gocloak.NewClient("http://localhost:8080")
 	ctx := context.Background()
-	token, err := client.LoginAdmin(ctx, "admin", "admin", "admin-realm")
+	token, err := client.LoginAdmin(ctx, "admin", "admin", "master")
 	if err != nil {
 		panic("Login failed:" + err.Error())
 	}
@@ -38,4 +69,22 @@ func KeyCloakTest() {
 		DisplayNameHTML: gocloak.StringP("Test Realm"),
 	}
 	client.CreateRealm(ctx, token.AccessToken, realm)
+}
+
+func KeyCloakGetRealm() {
+	client := gocloak.NewClient("http://localhost:8080", gocloak.SetAuthAdminRealms("admin/realms"), gocloak.SetAuthRealms("realms"))
+	restyClient := client.RestyClient()
+	restyClient.SetTLSClientConfig(&tls.Config{InsecureSkipVerify: true})
+	ctx := context.Background()
+	token, err := client.LoginAdmin(ctx, "admin", "admin", "master")
+	if err != nil {
+		panic("Login failed:" + err.Error())
+	}
+	realms, err := client.GetRealms(ctx, token.AccessToken)
+	if err != nil {
+		panic("GetRealm failed:" + err.Error())
+	}
+	for _, realm := range realms {
+		fmt.Printf("%s\n", *realm.Realm)
+	}
 }
--- model/KeyCloakRealm.go
+++ model/KeyCloakRealm.go
@@ -1,8 +1,77 @@
 package model
 
+import (
+	"context"
+	"fmt"
+
+	"github.com/Nerzal/gocloak/v11"
+)
+
 type KeyCloakRealm struct {
 	Name            string `json:"name"`
 	DisplayName     string `json:"displayName,omitempty" yaml:"displayName,omitempty"`
 	DisplayNameHTML string `json:"displayNameHTML,omitempty" yaml:"displayNameHTML,omitempty"`
 	Enabled         bool   `json:"enabled,omitempty"`
+}
+
+// does Realm Exist ?
+func (r *KeyCloakRealm) RealmExist(client gocloak.GoCloak, token string) bool {
+	realms, err := r.GetRealm(client, token)
+	return realms != nil && err == nil
+
+}
+
+// get keyCloak Realm
+func (r *KeyCloakRealm) GetRealm(client gocloak.GoCloak, token string) (*gocloak.RealmRepresentation, error) {
+	ctx := context.Background()
+	realms, err := client.GetRealm(ctx, token, r.Name)
+	if err != nil {
+		return nil, err
+	}
+	return realms, nil
+}
+
+// Create realm if not exist
+func (r *KeyCloakRealm) CreateRealm(client gocloak.GoCloak, token string) (string, bool, error) {
+	if r.RealmExist(client, token) {
+		return r.Name, true, nil
+	}
+	return r.createRealm(client, token)
+}
+
+func (r *KeyCloakRealm) createRealm(client gocloak.GoCloak, token string) (string, bool, error) {
+	ctx := context.Background()
+	str, err := client.CreateRealm(ctx, token, gocloak.RealmRepresentation{
+		Realm:           gocloak.StringP(r.Name),
+		DisplayNameHTML: gocloak.StringP(r.DisplayNameHTML),
+		DisplayName:     gocloak.StringP(r.DisplayName),
+		Enabled:         gocloak.BoolP(r.Enabled),
+	})
+	if err != nil {
+		fmt.Println("CreateRealm failed:" + err.Error())
+	}
+	return str, err == nil, err
+}
+
+// Upate realm if exist
+func (r *KeyCloakRealm) UpdateRealm(client gocloak.GoCloak, token string) error {
+	if !r.RealmExist(client, token) {
+		return fmt.Errorf("realm %s does not exist", r.Name)
+	}
+	return r.updateRealm(client, token)
+}
+
+// Update Realm
+func (r *KeyCloakRealm) updateRealm(client gocloak.GoCloak, token string) error {
+	ctx := context.Background()
+	err := client.UpdateRealm(ctx, token, gocloak.RealmRepresentation{
+		Realm:           gocloak.StringP(r.Name),
+		DisplayNameHTML: gocloak.StringP(r.DisplayNameHTML),
+		DisplayName:     gocloak.StringP(r.DisplayName),
+		Enabled:         gocloak.BoolP(r.Enabled),
+	})
+	if err != nil {
+		fmt.Println("UpdateRealm failed:" + err.Error())
+	}
+	return err
 }
